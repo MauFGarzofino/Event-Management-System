0433a74d01a45785292abb599d8b93f543fb88a9
MauFGarzo <mauricioflores123321@gmail.com>
2024-06-08 11:18:36 -0400

    Configure Swagger for API endpoint documentation

--- EventManagementSystemAPI/Program.cs
+++ EventManagementSystemAPI/Program.cs
@@ -12,11 +12,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;  
+});
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
