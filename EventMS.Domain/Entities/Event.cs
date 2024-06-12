c92e6795003eec15546fd50587cd96e42b8ccf96
btt <waltervaquera17@gmail.com>
2024-06-12 15:15:54 -0400

    Added endpoint for retrieving all events with basic validation

--- EventMS.Domain/Entities/Event.cs
+++ EventMS.Domain/Entities/Event.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace EventMS.Domain.Entities
 {
     public class Event
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [SwaggerSchema(Description = "The unique identifier for the event", ReadOnly = true)]
         public int Id { get; set; }
-        public string Title { get; set; }
+
+        [Required]
+        [SwaggerSchema(Description = "The title of the event")]
+        public string? Title { get; set; }
+
+        [StringLength(200)]
+        [SwaggerSchema(Description = "The description of the event")]
+        public string? Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        [SwaggerSchema(Description = "The date of the event", Format = "date")]
         public DateTime Date { get; set; }
-        public string Location { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Column(TypeName = "Time")]
+        [SwaggerSchema(Description = "The time of the event", Format = "time")]
+        public TimeSpan Time { get; set; }
+
+        [Required]
+        [SwaggerSchema(Description = "The location of the event")]
+        public string? Location { get; set; }
     }
 }
