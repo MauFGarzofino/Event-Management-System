e7b0abbda5c1c277735b14e714240a8ab3e515d0
btt <waltervaquera17@gmail.com>
2024-06-14 17:34:12 -0400

    Renamed EventManagementSystemAPI test proyect

--- EventManagementSystemAPITests/EventControllerTests.cs
+++ EventManagementSystemAPITests/EventControllerTests.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
-namespace EventManagementSystemAPITests
+namespace EventManagementSystemAPI.Tests
 {
     public class EventControllerTests
     {
