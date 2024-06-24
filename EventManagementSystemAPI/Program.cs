c436a08cd81078079a6072ed17fe58a49e4769d8
MauFGarzo <mauricioflores123321@gmail.com>
2024-06-24 11:56:23 -0400

    Refactor JWT configuration and update policies handling

--- EventManagementSystemAPI/Program.cs
+++ EventManagementSystemAPI/Program.cs
@@ -4,6 +4,7 @@ using EventManagementSystemAPI.Filters.validations;
 using EventManagementSystemAPI.MappingProfile;
 using EventManagementSystemAPI.Swagger;
 using EventManagementSystemAPI.Util;
+using EventMS.Infrastructure.Auth.TokenManagement;
 using EventMS.Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -16,7 +17,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configure DbContext with SQL Server
 var sqlConnection = builder.Configuration["ConnectionStrings:EMS:SqlDb"];
-
 builder.Services.AddSqlServer<ApplicationDbContext>(sqlConnection, options => options.EnableRetryOnFailure());
 
 builder.Services.AddRepositories();
@@ -55,67 +55,8 @@ var mapperConfig = new MapperConfiguration(cfg =>
 IMapper mapper = mapperConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
 
-// Authentication
-builder.Services.AddAuthentication(o =>
-{
-    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.Authority = "http://localhost:8080/realms/event-management-system/";
-    o.Audience = "ems-api";
-
-    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidAudiences = new[] { "master-realm", "account", "ems-api" }
-    };
-
-    o.Events = new JwtBearerEvents()
-    {
-        OnTokenValidated = context =>
-        {
-            var claims = context.Principal.Claims.ToList();
-
-            var resourceAccess = context.Principal.FindFirst("resource_access")?.Value;
-            if (resourceAccess != null)
-            {
-                var resourceAccessJson = System.Text.Json.JsonDocument.Parse(resourceAccess);
-                foreach (var resource in resourceAccessJson.RootElement.EnumerateObject())
-                {
-                    if (resource.Name == "ems-api")
-                    {
-                        foreach (var role in resource.Value.GetProperty("roles").EnumerateArray())
-                        {
-                            claims.Add(new Claim(ClaimTypes.Role, role.GetString()));
-                        }
-                    }
-                }
-            }
-
-            var appIdentity = new ClaimsIdentity(claims);
-            context.Principal.AddIdentity(appIdentity);
-
-            return Task.CompletedTask;
-        },
-        OnAuthenticationFailed = context =>
-        {
-            context.NoResult();
-            context.Response.StatusCode = 500;
-            context.Response.ContentType = "text/plain";
-            return context.Response.WriteAsync(context.Exception.ToString());
-        }
-    };
-
-    o.RequireHttpsMetadata = false;
-    o.SaveToken = true;
-});
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminClientRole", policy => policy.RequireRole("admin_client_role"));
-    options.AddPolicy("OrganizerClientRole", policy => policy.RequireRole("organizer_client_role"));
-    options.AddPolicy("UserClientRole", policy => policy.RequireRole("user_client_role"));
-});
+// JWT Authorization
+builder.Services.AddJwtBearerAuthorization(builder.Configuration);
 
 var app = builder.Build();
 
@@ -150,6 +91,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
