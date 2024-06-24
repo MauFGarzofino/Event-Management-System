c00f2d269f3644f77598bb9a709a0fa47d3307f0
FJFloresG <floresfernandajeanette@gmail.com>
2024-06-24 10:52:45 -0400

    "define relationships"

--- EventMS.Domain/Entities/Event.cs
+++ EventMS.Domain/Entities/Event.cs
@@ -12,11 +12,14 @@ namespace EventMS.Domain.Entities
 {
     public class Event
     {
+        [Key]
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime Date { get; set; }
         public TimeSpan Time { get; set; }
         public string? Location { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
--- EventMS.Domain/Entities/Ticket.cs
+++ EventMS.Domain/Entities/Ticket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,18 @@ namespace EventMS.Domain.Entities
 {
     public class Ticket
     {
+        [Key]
         public int Id { get; set; }
         public string TicketName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public int QuantityAvailable { get; set; }
         public DateTime SaleStartDate { get; set; }
         public DateTime SaleEndDate { get; set; }
         public int EventId { get; set; }
+
+        [ForeignKey("EventId")]
         public Event Event { get; set; }
     }
 }
