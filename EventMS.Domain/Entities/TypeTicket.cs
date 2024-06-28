a8d76427d58dc9c11e25301250ca4c52975a321b
FJFloresG <floresfernandajeanette@gmail.com>
2024-06-28 16:22:05 -0400

    "add and correct the code Type Tickets  "

--- EventMS.Domain/Entities/TypeTicket.cs
+++ EventMS.Domain/Entities/TypeTicket.cs
@@ -8,6 +8,7 @@ namespace EventMS.Domain.Entities
 {
     public class TypeTicket
     {
+        //
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
